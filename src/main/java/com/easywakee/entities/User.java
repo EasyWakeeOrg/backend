cc37cd609a975567ed3cb815dc9e2914cd9a0557
ptipito <pierre.nagellen@etu.emse.fr>
2017-01-07 17:30:36 +0100

    Entities

---
+++ src/main/java/com/easywakee/entities/User.java
@@ -0,0 +1,10 @@
+package com.easywakee.entities;
+
+import java.util.ArrayList;
+
+public class User {
+	
+	private int time;//the time param in minutes
+	private Address address;
+	private ArrayList<String> transport;
+}
--- src/main/java/com/easywakee/service/UserRepository.java
+++ src/main/java/com/easywakee/service/UserRepository.java
@@ -8,13 +8,13 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import com.easywakee.entities.User;
 
 @RepositoryRestResource(collectionResourceRel="users", path="users")
-public interface UserRepository extends MongoRepository<String, String> {
-    public List<String> findByFirstName(@Param("fn")String firstName);
-    public List<String> findByLastName(@Param("ln") String lastName);
-    public List<String> findByFirstNameAndLastName(@Param("fn") String firstName,@Param("ln") String lastName);
-    public String findByEmail(@Param("id") String email);
+public interface UserRepository extends MongoRepository<User, String> {
+    public List<User> findByFirstName(@Param("fn")String firstName);
+    public List<User> findByLastName(@Param("ln") String lastName);
+    public List<User> findByFirstNameAndLastName(@Param("fn") String firstName,@Param("ln") String lastName);
+    public User findByEmail(@Param("id") String email);
     public long deleteUserByEmail(@Param("id") String email);
 }
